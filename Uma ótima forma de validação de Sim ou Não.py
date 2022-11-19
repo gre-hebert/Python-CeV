63e93f3c1c85bced5e3bae33476cea09ec2b3688
Grégori Hebert <gre.hebert@gmail.com>
2022-11-19 10:46:01 -0300

    Update Uma ótima forma de validação de Sim ou Não.py

--- Uma ótima forma de validação de Sim ou Não.py
+++ Uma ótima forma de validação de Sim ou Não.py
@@ -1,6 +1,11 @@
 while True:
+    print('Olá, Mundo!')
+    while True:
         continuar = str(input('Quer continuar? [S/N]: ')).strip().upper()
         if continuar != 'S' and continuar != 'N':
             print('Opção Inválida!')
         else:
             break
+    if continuar == 'N':
+        break
+print('Tchau, mundo!')
