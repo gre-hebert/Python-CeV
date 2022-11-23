1c6e29d2db319772f8fd147d9e0a34a23e5d00e8
Grégori Hebert <gre.hebert@gmail.com>
2022-11-23 14:41:34 -0300

    Update Ex090 – Dicionário em Python.py

--- Meu exercícios/Ex090 – Dicionário em Python.py
+++ Meu exercícios/Ex090 – Dicionário em Python.py
@@ -1,6 +1,6 @@
 aluno = {'Nome': str(input('Nome: ')).strip().lower().capitalize(), 'Nota': float(input('Nota: '))}
-print(f'Nome do aluno é {aluno["Nome"]}.\nEle tirou {aluno["Nota"]}')
 if aluno['Nota'] >= 7:
-    print('Ele está \033[34mAPROVADO\033[m')
+    aluno['Situação'] = '\033[34mAPROVADO\033[m'
 else:
-    print('Ele está \033[31mREPROVADO\033[m')
+    aluno['Situação'] = '\033[31mREPROVADO\033[m'
+print(f'Nome do aluno é {aluno["Nome"]}.\nEle tirou {aluno["Nota"]}.\nEle está {aluno["Situação"]}')
