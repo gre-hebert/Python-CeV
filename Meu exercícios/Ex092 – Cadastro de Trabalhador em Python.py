527e55bd768aa5ebc4eb92b81ba84d2ac7eb18e0
Grégori Hebert <gre.hebert@gmail.com>
2022-11-24 23:30:47 -0300

    Update Ex092 – Cadastro de Trabalhador em Python.py

--- Meu exercícios/Ex092 – Cadastro de Trabalhador em Python.py
+++ Meu exercícios/Ex092 – Cadastro de Trabalhador em Python.py
@@ -7,3 +7,11 @@ if cadastro['CTPS'] == 0:
     print('-=' * 30)
     for keys, values in cadastro.items():
         print(f'{keys} = {values}')
+else: 
+    cadastro['Contratação'] = int(input('Ano de Contratação: '))
+    cadastro['Salário'] = float(input('Salário: R$'))
+    cadastro['Aposentadoria'] = cadastro['Contratação'] + 35 - (date.today().year - cadastro['Idade'])
+    print('-=' * 30)
+    for keys, values in cadastro.items():
+        print(f'{keys} = {values}')
+        
