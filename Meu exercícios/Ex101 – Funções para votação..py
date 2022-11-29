fe3f143df7272659b6ffe05df35fe7a472029a47
Grégori Hebert <gre.hebert@gmail.com>
2022-11-28 23:52:26 -0300

    Update Ex101 – Funções para votação..py

--- Meu exercícios/Ex101 – Funções para votação..py
+++ Meu exercícios/Ex101 – Funções para votação..py
@@ -3,8 +3,10 @@ def voto(ano):
     idade = date.today().year - ano
     if idade < 16:
         return print('Não pode votar')
-        
-    
+    elif idade < 18 or idade > 65:
+        return print('Voto facultativo')
+    elif idade < 65:
+        return print('Voto obrigatória') 
 
 
 data = int(input('Em que ano você nasceu: '))
