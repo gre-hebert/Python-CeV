5df75ede3c35339d17272b5b19e03ccd622094d7
Grégori Hebert <96366701+gre-hebert@users.noreply.github.com>
2022-12-20 14:11:07 -0300

    Aula 106

--- Exercicios/Ex106 – Interactive helping system in Python.py
+++ Exercicios/Ex106 – Interactive helping system in Python.py
@@ -9,4 +9,28 @@ c = ('\033[m',        # 0 - sem cortes
     );
 
 def ajuda(com):
-    título(f'Acessando o manual do comando ')
+    título(f'Acessando o manual do comando \'{com}\'', 4)
+    print(c[6], end='')
+    help(com)
+    print(c[0], end='')
+    sleep(2)
+
+def título(msg, cor=0):
+    tam = len(msg) + 4
+    print(c[cor], end='')
+    print('~' * tam)
+    print(f'  {msg}')
+    print('~' * tam)
+    print(c[0], end='')
+    sleep(1)
+
+# Programa Principal
+comando = ''
+while True:
+    título('SISTEMA DE AJUDA PyHELP', 2)
+    comando = str(input("Função e Biblioteca > "))
+    if comando.upper() == 'FIM':
+        break 
+    else:
+        ajuda(comando)
+título('ATÉ LOGO', 1)
